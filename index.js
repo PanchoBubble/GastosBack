c135c07ab14970167f5f6f785464c660ca6652e5
panchopure <30704190+panchopure@users.noreply.github.com>
2019-07-23 22:46:40 -0300

    Cambios juli

---
+++ index.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+ 
+mongoose.connect('mongodb://localhost/mongoose_basics', function (err) {
+ 
+   if (err) throw err;
+ 
+   console.log('Successfully connected');
+ 
+});
+
+var bookSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    title: String,
+    summary: String,
+    isbn: String,
+    thumbnail: Buffer,
+    author: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Author'
+    },
+    ratings: [
+        {
+            summary: String,
+            detail: String,
+            numberOfStars: Number,
+            created: { 
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    created: { 
+        type: Date,
+        default: Date.now
+    }
+});
